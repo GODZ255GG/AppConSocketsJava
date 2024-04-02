e7659fcbf33ef1af2628bef425c02b55ad3084ce
René García <100447765+GODZ255GG@users.noreply.github.com>
2024-04-01 20:04:00 -0600

    Primer commit

---
+++ AppSocketsJavaHolaMundo/Cliente.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Cliente {
+    public static void main(String[] args){
+        // Establece el punto a utilizar
+        int puerto = 8080;
+        try{
+            // Cuando un cliente se conecta se abre el puerto a escuchar conexiones de cliente
+            // Si el servidor responde, AQUI SE CREA EL SOCKET EN EL SO
+            Socket cs = new Socket("localhost", puerto);
+
+            // Establece el stream de salida
+            PrintWriter out = new PrintWriter(cs.getOutputStream(), true);
+            //Establece el stream de entrada
+            BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+
+            // Variables para leer los datos
+            String linearecibida;
+            // Iniciamos la comunicación
+            // Leemos líneas hasta recibir "Adios"
+            while (!(linearecibida = in.readLine()).equalsIgnoreCase("Adios")){
+                System.out.println("Servidor: "+ linearecibida);
+            }
+
+            // Envía la despedida al servidor
+            out.println("Recepción de datos correcta...");
+
+            // Cierra las conexiones
+            out.close();
+            in.close();
+            cs.close();
+        }catch (IOException ex){
+            System.out.println(ex);
+        }
+    }
+}
